e726af1f353476e30b81fba87b9ce4e7a8021134
whereistt <mac@pc-129-31.customer.ask4.lan>
2021-03-04 00:39:14 +0000

    10

---
+++ Assets/Scripts/Targetcamera.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+    public class Targetcamera : MonoBehaviour {  
+	public Transform target;
+	public float theta;
+
+
+	void Update(){
+		transform.position = (new Vector3 (target.position.x + (-7) * Mathf.Sin (target.eulerAngles.y*(Mathf.PI/180) ), 3+target.position.y ,target.position.z + (-7) * Mathf.Cos (target.eulerAngles.y*(Mathf.PI/180) )));
+        transform.rotation = Quaternion.Euler(18.0f, target.eulerAngles.y, 0.0f);
+	}
+}
