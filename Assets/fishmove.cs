288ac29e4febe10c1026a906402064f6b51b88a0
whereistt <mac@Raintimes-MacBook-Pro.local>
2021-03-07 22:29:33 +0000

    4

---
+++ Assets/fishmove.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class fishmove : MonoBehaviour {
+
+    private float moveSpeed = 3f;
+    private float scrollSpeed = 5f;
+
+    void Update () {
+        if (Input.GetAxisRaw("Horizontal") != 0 || Input.GetAxisRaw("Vertical") != 0) {
+            transform.position += moveSpeed * new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
+        }
+
+        if (Input.GetAxis("Mouse ScrollWheel") != 0) {
+            transform.position += scrollSpeed * new Vector3(0, -Input.GetAxis("Mouse ScrollWheel"), 0);
+        }
+    }
+
+}
